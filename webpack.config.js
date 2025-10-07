37a1a426d89013e617f8fb81f190005ad7e0e3bc
Daniel Roberts <ademan555@gmail.com>
2025-10-07 16:10:43 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'production',
+	entry: {
+		'main': './src/main.js',
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname + 'dist'),
+	},
+};
